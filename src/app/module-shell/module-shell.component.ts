c5157733cf7e26360a3fa958d44884958a62a573
Remo Loetscher <remo.loetscher@bison-group.com>
2019-01-14 11:36:54 +0100

    Input/Output added

--- src/app/module-shell/module-shell.component.ts
+++ src/app/module-shell/module-shell.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Output, Input, EventEmitter, ElementRef, OnInit,  AfterViewInit,ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,18 @@ import { Router } from '@angular/router';
   templateUrl: './module-shell.component.html',
   styleUrls: ['./module-shell.component.scss'],
 })
-export class ModuleShellComponent implements OnInit {
+export class ModuleShellComponent implements OnInit,  AfterViewInit{
+
+  @Output() cancelWfl = new EventEmitter<string>();
+  @Input() activityConf:any;
+  @Input() activityConfId:string;
+  @Input() testInput = "dummy";
+
+  private id: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private el: ElementRef
   ) { }
 
   ngOnInit() {
@@ -17,4 +25,17 @@ export class ModuleShellComponent implements OnInit {
     this.router.navigate(['/page1']);
   }
 
+  ngAfterViewInit() {
+    if (this.activityConfId) {
+      this.id = this.activityConfId;
+    }
+  }
+
+  public cancelWorkflowNG() {
+    this.cancelWfl.emit("cancel workflow (EventEmmitter)");
+  }
+
+  public cancelWorkflow() {
+    this.el.nativeElement.dispatchEvent(new CustomEvent('CustomWebcomponentEvent', { bubbles: true, cancelable: true, detail: 'cancel workflow (dispatched)' }));
+  }
 }
